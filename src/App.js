e37c878feaa619c3bb8602584e682876878c321b
sharath <sharathth17@gmail.com>
2025-02-25 12:34:28 +0530

    Commit_SignIn page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-lg font-bold text-red-400">
+      Hello everyone!!!
+    </div>
+  );
+}
+
+export default App;
